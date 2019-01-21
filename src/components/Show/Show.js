1a428dffd3c896fdb5bc6929e6d56e9b20045616
Zhuravlev Denis <zhuravlevdenis94@gmail.com>
2019-01-21 08:36:52 +0300

    Second commit

--- src/components/Show/Show.js
+++ src/components/Show/Show.js
@@ -1,35 +1,55 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import {getShowInfo} from './../../api';
+import { getShowInfo } from './../../api';
 
 class Show extends Component {
   state = {
-    showId: "",
+    showId: '',
     data: null
   };
 
-  static getDerivedStateFromProps(nextProps, prevState){
+  static getDerivedStateFromProps(nextProps, prevState) {
+    if (nextProps.showId === prevState.showId) return null;
+
     return {
-      showId: nextProps.showId,
-    }
+      showId: nextProps.showId
+    };
   }
 
   componentDidUpdate(prevProps, prevState) {
-    getShowInfo(this.state.showId)
-    .then(parseResponse => console.log(parseResponse))
-
+    if (prevProps.showId === this.props.showId) return;
+    getShowInfo(this.state.showId).then(parseResponse =>
+      this.setState({
+        data: parseResponse
+      })
+    );
   }
 
   render() {
-    const {showId, data} = this.state;
-
+    const { showId, data } = this.state;
+    console.log(data);
     let info = '';
-    if(!data){ // нет данных
-      if(showId) info = 'Загрузка шоу с id ' + showId;
-      else info = 'Шоу не выбрано'
+    if (!data) {
+      if (showId) info = 'Загрузка шоу с id ' + showId;
+      else info = 'Шоу не выбрано';
+
+      return <p className="show-inforation t-show-info">{info}</p>;
     }
- 
-    return <p className="show-inforation t-show-info">{ info }</p>;
+
+    return (
+       <div className="show">
+          <img alt={data.name} className="show-image" src={data.image.medium} />
+          <h2 className="show-label t-show-name">{data.name}</h2>
+          <p className="show-text t-show-genre">
+            <b>Жанр: </b>
+            {data.genres.join(', ')}
+          </p>
+          <p
+            className="show-text t-show-summary"
+            dangerouslySetInnerHTML={{ __html: data.summary }}
+          />
+        </div>
+    );
   }
 }
 

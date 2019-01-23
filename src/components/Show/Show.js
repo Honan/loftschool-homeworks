230a63f390c1924e55851e4cf5e3891104067be0
Zhuravlev Denis <zhuravlevdenis94@gmail.com>
2019-01-23 16:16:34 +0300

    done

---
+++ src/components/Show/Show.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+import { getShowInfo } from './../../api';
+
+class Show extends Component {
+  state = {
+    showId: '',
+    data: null
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const {showId} = this.props
+    if (showId !== prevProps.showId) {
+      getShowInfo(showId).then(data =>
+        this.setState({
+          data,
+          showId
+        })
+      );
+    }
+  }
+
+  render() {
+    const { showId, data } = this.state;
+
+    let info = '';
+    if (!data) {
+      if (showId) info = 'Загрузка шоу с id ' + showId;
+      else info = 'Шоу не выбрано';
+
+      return <p className="show-inforation t-show-info">{info}</p>;
+    }
+
+    return (
+      <div className="show">
+        <img alt={data.name} className="show-image" src={data.image.medium} />
+        <h2 className="show-label t-show-name">{data.name}</h2>
+        <p className="show-text t-show-genre">
+          <b>Жанр: </b>
+          {data.genres.join(', ')}
+        </p>
+        <p
+          className="show-text t-show-summary"
+          dangerouslySetInnerHTML={{ __html: data.summary }}
+        />
+      </div>
+    );
+  }
+}
+
+export default Show;
